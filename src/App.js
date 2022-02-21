c7f6964fa239721914f5d4198c1e1eeab5ce3868
reinhard9921 <rengelbrecht@sts3d.co.za>
2022-02-21 22:33:19 +0200

    Validation

--- src/App.js
+++ src/App.js
@@ -1,118 +1,111 @@
+import "./App.css";
+import React from "react";
+import Axios from "axios";
 
-import './App.css';
-import React from 'react';
-import Axios from 'axios';
-import validator from 'validator';
-const useState = React.useState
+const useState = React.useState;
 
+function AddUserApp() {
+  const [_userName, set_userName] = useState();
+  const [_userHeight, set_userHeight] = useState();
+  const [_userEmail, set_userEmail] = useState();
+  const [emailError, setEmailError] = useState();
+  const [heightError, setheightError] = useState();
+  const [nameError, setnameError] = useState();
 
+  function emailValidation(e) {
+    e.preventDefault();
 
-function AddUserApp(props) {
-  
-
-  const [_userName, set_userName] = useState()
-  const [_userHeight, set_userHeight] = useState()
-  const [_userEmail, set_userEmail] = useState()
-  const [emailError, setEmailError] = useState()
-  const [heightError, setheightError] = useState()
-  const [nameError, setnameError] = useState()
-
-  function emailValidation(e){
-    
-    e.preventDefault()
-    const regexemail = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-    if(!_userEmail || regexemail.test(_userEmail) === false){
-      setEmailError('Enter valid email');
+    const regexemail =
+      // eslint-disable-next-line no-useless-escape
+      /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    if (!_userEmail || regexemail.test(_userEmail) === false) {
+      setEmailError("Enter valid email");
     }
 
     const regexNumber = /^[0-9\b]+$/;
-    if(!_userHeight || regexNumber.test(_userHeight) === false){
-    setheightError('Enter valid number');
+    if (!_userHeight || regexNumber.test(_userHeight) === false) {
+      setheightError("Enter valid number");
     }
 
-    if(!_userName){
-      setnameError('Enter a name');
+    if (!_userName) {
+      setnameError("Enter a name");
     }
-    
-    if(_userName && _userHeight && regexNumber.test(_userHeight) === true && _userEmail && regexemail.test(_userEmail) === true)
-    {
+
+    if (
+      _userName &&
+      _userHeight &&
+      regexNumber.test(_userHeight) === true &&
+      _userEmail &&
+      regexemail.test(_userEmail) === true
+    ) {
       handleSubmit();
-
     }
-    
-
-    
-
-
-
-
   }
 
   function handleSubmit() {
-    const user = {
-      name: _userName,
-      height: _userHeight,
-      email: _userEmail
-    }
     var params = new URLSearchParams();
-    params.append('name', _userName);  
-    params.append('height', _userHeight);  
-    params.append('email', _userEmail);  
-    Axios.post('https://serverside-project-dev.herokuapp.com/api/insertuser',params 
-    )
+    params.append("name", _userName);
+    params.append("height", _userHeight);
+    params.append("email", _userEmail);
+    Axios.post(
+      "https://serverside-project-dev.herokuapp.com/api/insertuser",
+      params
+    );
 
-    set_userName("")
-    set_userHeight("")
-    set_userEmail("")
-    setEmailError("")
-    setheightError("")
-    setnameError("")
+    set_userName("");
+    set_userHeight("");
+    set_userEmail("");
+    setEmailError("");
+    setheightError("");
+    setnameError("");
   }
 
-
-
   return (
-    
-<html>
-    <head>
-       
-      </head>
-    <body>
-    <div className="App">
-      <div className="page_wrap">
-        <form onSubmit={emailValidation}>
-        <br/>
+    <div className="page_wrap">
+      <form onSubmit={emailValidation}>
+        <br />
         <h2>Please enter your height</h2>
         <h3>For a email with your high compared to our average</h3>
-        <label htmlFor="name">Name:</label><br></br>
-        <input type="text" value={_userName} placeholder="John" onChange={e =>set_userName(e.target.value)}></input><br></br>
-        <span style={{
-          color: 'red',
-        }}>{
-          nameError}</span><br></br>
-        <label htmlFor="height">Height (cm):</label><br></br>
-        <input type="text"  value={_userHeight} placeholder="178" onChange={e =>set_userHeight(e.target.value)}></input><br/>
-        <span style={{
-          color: 'red',
-        }}>{
-          heightError}</span>
-        <br></br>
-        <label htmlFor="email">Email:</label><br></br>
-        <input type="text" value={_userEmail} placeholder="john04@gmail.com" onChange={(e) =>set_userEmail(e.target.value)}></input><br/>
-        <span style={{
-          color: 'red',
-        }}>{emailError}</span><br></br><br></br>
+        <label htmlFor="name">Name:</label>
+        <br />
+        <input
+          type="text"
+          value={_userName}
+          placeholder="John"
+          onChange={(e) => set_userName(e.target.value)}
+        ></input>
+        <br />
+        <span>{nameError}</span>
+        <br />
+        <label htmlFor="height">Height (cm):</label>
+        <br />
+        <input
+          type="text"
+          value={_userHeight}
+          placeholder="178"
+          onChange={(e) => set_userHeight(e.target.value)}
+        ></input>
+        <br />
+        <span>{heightError}</span>
+        <br />
+        <label htmlFor="email">Email:</label>
+        <br />
+        <input
+          type="text"
+          value={_userEmail}
+          placeholder="john04@gmail.com"
+          onChange={(e) => set_userEmail(e.target.value)}
+        ></input>
+        <br />
+        <span>{emailError}</span>
+        <br />
+        <br />
 
         <button variant="primary">Submit Info</button>
-        <br/><br/>
-        </form>
-       </div>
+        <br />
+        <br />
+      </form>
     </div>
-    
-</body>
-</html>
   );
 }
-
-
 export default AddUserApp;
